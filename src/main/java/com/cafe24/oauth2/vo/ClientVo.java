4f4bc35069299b129963cf2a3df8bc6a86b8a3dd
shkim <skok1025@naver.com>
2021-12-04 22:44:03 +0900

    lombok 적용

--- src/main/java/com/cafe24/oauth2/vo/ClientVo.java
+++ src/main/java/com/cafe24/oauth2/vo/ClientVo.java
@@ -1,41 +1,18 @@
 package com.cafe24.oauth2.vo;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class ClientVo {
 	private String id;
 	private String secret;
 	private String[] callbackUrl;
 	private String[] grantType;
-	private String[] scopes;
-	
-	public ClientVo(String id, String secret, String[] callbackUrl, String[] grantType, String[] scopes) {
-		this.id =id;
-		this.secret = secret;
-		this.callbackUrl = callbackUrl;
-		this.grantType = grantType;
-		this.scopes = scopes;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getSecret() {
-		return secret;
-	}
-
-	public String[] getCallbackUrl() {
-		return callbackUrl;
-	}
-
-	public String[] getGrantType() {
-		return grantType;
-	}
-
-	public String[] getScopes() {
-		return scopes;
-	}
-
-	
+	private String[] scopes;	
 }
